d33d186de77bd7865f23182b459c12b074e8f665
gokul-scalent <gokul@scalent.io>
2022-03-25 12:56:41 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+
+}
+
+func add(a, b int) int {
+	if a == 0 {
+		b = b + 1
+	}
+	c := a + b
+	return c
+}
---
+++ main_test.go
@@ -0,0 +1,43 @@
+package main
+
+import "testing"
+
+func Test_add(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "both positive",
+			args: args{0, 3},
+			want: 8,
+		},
+		{
+			name: "both negative",
+			args: args{-5, -3},
+			want: -8,
+		},
+		{
+			name: "first positive, second negative",
+			args: args{5, -3},
+			want: 2,
+		},
+		{
+			name: "first negative, second positiive",
+			args: args{-5, 3},
+			want: -7,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := add(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
